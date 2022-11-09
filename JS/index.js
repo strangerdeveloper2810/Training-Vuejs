51864eaa99178cf7f85dc0e2ff6b8dad093e834b
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-11-10 04:07:15 +0700

    learn binding-attribute in vuejs

--- JS/index.js
+++ JS/index.js
@@ -15,3 +15,15 @@ Vue.createApp({
     };
   },
 }).mount("#interpolation");
+
+Vue.createApp({
+  data() {
+    return {
+      ImgName: "Lisa",
+      ImgUrl:
+        "https://vcdn1-giaitri.vnecdn.net/2020/11/03/lisa-6-1604394415.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=2uX86_Jm0qtPjfD7wkRD3w",
+      linkInfo:
+        "https://vnexpress.net/lisa-vung-danh-hieu-sao-dep-nhat-chau-a-4186468.html",
+    };
+  },
+}).mount("#binding-attribute");
