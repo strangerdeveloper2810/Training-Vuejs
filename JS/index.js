e521cff7958ffe6f0330b2101d8c5b111328cbcb
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-11-10 03:43:32 +0700

    learn interpolation in vuejs

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+// Vue.createApp({
+//   data: function () {
+//     return {
+//         nameUser: "Stranger",
+//         className: "VueJS 01"
+//     };
+//   },
+// }).mount("#interpolation");
+
+Vue.createApp({
+  data() {
+    return {
+      nameUser: "Stranger",
+      className: "VueJS 01",
+    };
+  },
+}).mount("#interpolation");
